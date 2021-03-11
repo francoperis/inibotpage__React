2e188efa7744e22b04002e2a37af2378a64d0334
francoperis <franco.peris@gmail.com>
2021-03-11 13:47:54 -0300

    header y hero con banner texto e img

---
+++ src/componentes/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './style/header.css'
+import Logopage from './images/paglogo.png'
+class Header extends React.Component{
+    render() {
+        return (
+        <div className="header">
+            <div className="header-img">
+                <img src = {Logopage} alta ="InibotLogo" />
+            </div>
+
+            <div>
+                <nav className="navbar">
+                    <ul className="navbar-ul">
+                        <li className="navbar-ul__li">
+                            <a href="#">ABOUT US</a>
+                        </li>
+                        <li className="navbar-ul__li">
+                            <a href="#">TECNOLOGIA</a>
+                        </li>
+                        <li className="navbar-ul__li">
+                            <a href="#">FORMULARIO</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default Header;
---
+++ src/componentes/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './style/hero.css'
+import FiguraGrua from './images/grua.png'
+import Header from './Header.js'
+
+class Hero extends React.Component{
+    render() {
+        return(
+            <div className="banner">
+                <Header />
+                <div className="main">
+                    <div className="main-hero">
+                        <h1 className="main-hero__h1">DESARROLLO <br/> DE SOFTWARE</h1>
+                        <img className="main-hero__img" src={FiguraGrua} alt="Figura-grua" ></img>
+                    </div>
+                </div>
+            </div>
+        )   
+        
+    }
+}
+
+
+export default Hero;
